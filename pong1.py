a3c10db8d3db007a192612378fd6fa8d0306e38c
codingscode <heronnestg@hotmail.com>
2021-12-05 23:43:40 -0300

    parte 1

---
+++ pong1.py
@@ -0,0 +1,22 @@
+
+#from tkinter import *
+import turtle
+
+
+janela = turtle.Screen()
+janela.title('joguinho ping pong')
+janela.bgcolor('green')
+janela.setup(width=800, height=600)
+janela.tracer(0)
+
+# loop do jogo principal
+while True:
+    janela.update()
+
+
+
+
+
+
+
+
